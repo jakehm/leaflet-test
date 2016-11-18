56da6efca3d02e5077d1727fe77067d234187dff
Jacob Mortelliti <jacob.harrowmortelliti@maine.eu>
2016-11-18 08:19:04 -0500

    gave up on snackbar, just made it a pure js alert

--- src/app/App.js
+++ src/app/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
     lat: 51.505,
     lng: -0.09,
     mapKey: Math.random(),
-    snackbarActive: false
   }
 
   componentDidMount () {
@@ -26,8 +25,7 @@ class App extends React.Component {
   }
 
   handleGeolocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
+    navigator.geolocation.getCurrentPosition(
         (position) => {
           this.setState({
             lat: position.coords.latitude,
@@ -35,16 +33,9 @@ class App extends React.Component {
             mapKey: Math.random()
           })
         }, (err) => {
-          this.setState({snackbarActive: true})
+          alert("Geolocation did not work: " + err)
         }
       )
-    } else {
-      this.setState({snackbarActive: true})
-    }
-  }
-
-  handleSnackbarToggle = () => {
-    this.setState({snackbarActive: !this.state.snackbarActive})
   }
 
   render () {
@@ -66,26 +57,7 @@ class App extends React.Component {
               onClick={this.handleGeolocation}
             />
           </Control>
-        </Map> 
-        <div style={{
-          zIndex: 99999999,
-          position: 'absolute',
-          left: 0,
-          top: 90
-        }}>
-          <button>hi </button>
-        <Snackbar
-          theme={snackbarTheme}
-          action='Dismiss'
-          active={this.state.snackbarActive}
-          icon='my_location'
-          label='Geolocation is not enabled on your device.'
-          timeout={2000}
-          onClick={this.handleSnackbarToggle}
-          onTimeout={this.handleSnackbarToggle}
-          type='warning'
-        />
-      </div>
+        </Map>
       </div>
     )
   }
